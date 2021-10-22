5bb1aeebdfd4c32296656c86f90c81bd6cb2d8df
ADRIEL LOURENÇO <21276092@CFP127ALUNO.LOCAL>
2021-10-22 16:52:39 -0300

    Código refatorado.

--- src/br/senai/sp/jandira/model/Imc.java
+++ src/br/senai/sp/jandira/model/Imc.java
@@ -1,39 +1,29 @@
 package br.senai.sp.jandira.model;
 
 public class Imc {
-	
-	private double imc;
+
 	private String status;
-	
-	public void calcularImc(double altura, double peso) {
-		imc = peso/(altura*altura);
-}
-	
-	public double getImc() {
-		return imc;
+
+	public double calcularImc(double altura, double peso) {
+		return peso / (altura * altura);
 	}
-	
+
 	public void obterStatus(double imc) {
-		if (imc<=18.5) {
+		if (imc < 18.5) {
 			status = "Abaixo do Peso!";
-		}
-		else if (imc >= 18.6 && imc <= 24.9) {
+		} else if (imc >= 18.5 && imc < 25.0) {
 			status = "Peso Ideal! (Parabéns!)";
-		}
-		else if (imc >= 25.0 && imc <= 29.9) {
+		} else if (imc >= 25.0 && imc < 30.0) {
 			status = "Levemente Acima do Peso!";
-		}
-		else if (imc >= 30.0 && imc <= 34.9) {
+		} else if (imc >= 30.0 && imc < 35.0) {
 			status = "Obesidade Grau I!";
-		}
-		else if (imc >= 35.0 && imc <= 39.9) {
+		} else if (imc >= 35.0 && imc < 40.0) {
 			status = "Obesidade Grau II (Severa)!";
-		}
-		else if (imc > 40.0 ) {
+		} else {
 			status = "Obesidade Grau III (Mórbida)!";
 		}
 	}
-	
+
 	public String getStatus() {
 		return status;
 	}
--- src/br/senai/sp/jandira/ui/Tela.java
+++ src/br/senai/sp/jandira/ui/Tela.java
@@ -16,100 +16,106 @@ public class Tela {
 
 	public void criarTela() {
 
+		// Minhas Fontes da Tela
+		Font fontTitle = new Font("Arial Black", Font.BOLD, 30);
+		Font fontSubTitle = new Font("Arial Black", Font.PLAIN, 20);
+		Font fontResultados = new Font("Arial", Font.PLAIN, 20);
+
+		// Cores da Minha Tela
+		Color blue = new Color(69, 32, 214);
+		Color black = new Color(000);
+		Color green = new Color(137, 227, 68);
+
 		JFrame minhaTela = new JFrame();
 		minhaTela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		minhaTela.setSize(400,525);
+		minhaTela.setSize(420, 525);
 		minhaTela.setTitle("Cálculo de IMC");
 		minhaTela.setLayout(null);
-		
+
 		JLabel lblTiulo = new JLabel();
 		lblTiulo.setText("Cálculo de IMC");
 		lblTiulo.setBounds(20, 20, 300, 30);
-		lblTiulo.setFont(new Font("Arial Black", Font.PLAIN, 30));
-		lblTiulo.setForeground(new Color(69, 32, 214));
-		
+		lblTiulo.setFont(fontTitle);
+		lblTiulo.setForeground(blue);
+
 		JLabel lblDigiteSeuPeso = new JLabel();
 		lblDigiteSeuPeso.setText("Seu Peso (Kg):");
 		lblDigiteSeuPeso.setBounds(20, 80, 90, 30);
-		
+
 		JTextField txtDigiteSeuPeso = new JTextField();
 		txtDigiteSeuPeso.setBounds(110, 80, 80, 30);
 		txtDigiteSeuPeso.getText();
-		
+
 		JLabel lblDigiteSuaAltura = new JLabel();
 		lblDigiteSuaAltura.setText("Sua Altura (m):");
 		lblDigiteSuaAltura.setBounds(20, 120, 90, 30);
-		
+
 		JTextField txtDigiteSuaAltura = new JTextField();
 		txtDigiteSuaAltura.setBounds(110, 120, 80, 30);
 		txtDigiteSuaAltura.getText();
-		
+
 		JButton btnCalcular = new JButton();
 		btnCalcular.setText("CALCULAR");
 		btnCalcular.setBounds(37, 160, 120, 50);
-		
+
 		JLabel lblResultados = new JLabel();
 		lblResultados.setText("Resultados:");
 		lblResultados.setBounds(20, 250, 150, 30);
-		lblResultados.setFont(new Font("Arial Black", Font.PLAIN, 20));
-		lblResultados.setForeground(new Color(0000));
-		
+		lblResultados.setFont(fontSubTitle);
+		lblResultados.setForeground(black);
+
 		JLabel lblValorImc = new JLabel();
 		lblValorImc.setText("Valor IMC:");
 		lblValorImc.setBounds(20, 300, 70, 30);
-		
+
 		JLabel lblResultadoImc = new JLabel();
 		lblResultadoImc.setText("00.0");
 		lblResultadoImc.setBounds(125, 300, 300, 30);
-		lblResultadoImc.setFont(new Font("Arial", Font.PLAIN, 20));
-		lblResultadoImc.setForeground(new Color(137, 227, 68));
-		
+		lblResultadoImc.setFont(fontResultados);
+		lblResultadoImc.setForeground(green);
+
 		JLabel lblEstadoImc = new JLabel();
 		lblEstadoImc.setText("Estado IMC:");
 		lblEstadoImc.setBounds(20, 340, 70, 30);
-		
+
 		JLabel lblResultadoEstado = new JLabel();
 		lblResultadoEstado.setText("Status");
 		lblResultadoEstado.setBounds(125, 340, 300, 20);
-		lblResultadoEstado.setFont(new Font("Arial", Font.PLAIN, 20));
-		lblResultadoEstado.setForeground(new Color(137, 227, 68));
-	
+		lblResultadoEstado.setFont(fontResultados);
+		lblResultadoEstado.setForeground(green);
+
 		minhaTela.getContentPane().add(lblTiulo);
 		minhaTela.getContentPane().add(lblDigiteSeuPeso);
 		minhaTela.getContentPane().add(lblDigiteSuaAltura);
 		minhaTela.getContentPane().add(txtDigiteSeuPeso);
 		minhaTela.getContentPane().add(txtDigiteSuaAltura);
 		minhaTela.getContentPane().add(btnCalcular);
-		minhaTela.getContentPane().add(lblResultados);	
+		minhaTela.getContentPane().add(lblResultados);
 		minhaTela.getContentPane().add(lblValorImc);
 		minhaTela.getContentPane().add(lblEstadoImc);
 		minhaTela.getContentPane().add(lblResultadoImc);
-		minhaTela.getContentPane().add(lblResultadoEstado); 
-		
+		minhaTela.getContentPane().add(lblResultadoEstado);
+
 		minhaTela.setVisible(true);
 
-				
-		
 		btnCalcular.addActionListener(new ActionListener() {
-			
+
 			@Override
 			public void actionPerformed(ActionEvent e) {
-				
-				String Speso = txtDigiteSeuPeso.getText();
-				double peso = Double.parseDouble(Speso);
-				
-				String Saltura = txtDigiteSuaAltura.getText();
-				double altura = Double.parseDouble(Saltura);
 
-				
+				String stringPeso = txtDigiteSeuPeso.getText();
+				double peso = Double.parseDouble(stringPeso);
+
+				String stringAltura = txtDigiteSuaAltura.getText();
+				double altura = Double.parseDouble(stringAltura);
+
 				Imc calculo = new Imc();
-		
-				calculo.calcularImc(altura, peso);
-				calculo.obterStatus(calculo.getImc());
+				double imc = calculo.calcularImc(altura, peso);
+				calculo.obterStatus(imc);
 				lblResultadoEstado.setText((calculo.getStatus()));
-				lblResultadoImc.setText(Double.toString(calculo.getImc()));
+				lblResultadoImc.setText(Double.toString(imc));
 			}
 		});
-		
+
 	}
 }
